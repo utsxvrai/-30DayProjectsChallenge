aa090243d7540030f9ba821dfd5e94cae349b6aa
Utsav Rai <utsavrai2115@gmail.com>
2023-05-08 18:54:31 +0530

    added milliseconds option

--- Project 1/index.js
+++ Project 1/index.js
@@ -1,6 +1,7 @@
 const hr = document.getElementById("hour")
 const min = document.getElementById("minute")
 const sec = document.getElementById("second")
+const milsec = document.getElementById("milisecond")
 const ap = document.getElementById("ampm")
 
 
@@ -8,6 +9,7 @@ function updateClock(){
     let h = new Date().getHours()
     let m = new Date().getMinutes()
     let s = new Date().getSeconds()
+    let ms = new Date().getMilliseconds();
     let a = "AM"
 
     if(h > 12){
@@ -17,15 +19,17 @@ function updateClock(){
     h = h<10 ? "0"+h : h;
     m = m<10 ? "0"+m : m;
     s = s<10 ? "0"+s : s;
+    ms = ms < 10 ? "00" + ms : (ms < 100 ? "0" + ms : ms);
 
     hr.innerText = h;
     min.innerText = m;
     sec.innerText=s;
+    milsec.innerText=ms;
     ap , (innertext = a);
 
     setTimeout(()=> {
         updateClock();
-    },1000)
+    },1)
 
 
 }
